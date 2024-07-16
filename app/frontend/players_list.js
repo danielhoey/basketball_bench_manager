35d9e7ad2dfdc7f26c5a69ac8716a78d2d7d4ebc
Daniel Hoey <danielhoey1@gmail.com>
2024-07-16 10:05:51 +0800

    Show errors

--- app/frontend/players_list.js
+++ app/frontend/players_list.js
@@ -2,14 +2,20 @@ import {createApp} from "vue";
 
 export default createApp({
     data() {
-        return { count: 12, newPlayer: {}, players: [] }
+        return { newPlayer: {}, players: [], errors: {} }
     },
     methods: {
         save(event) {
+            this.errors = {};
+            const matchingNumber = this.players.find((p) => p.number == this.newPlayer.number);
+            if (matchingNumber) {
+                this.errors = {number: "Number already taken"};
+                return;
+            }
+
             this.players.push(this.newPlayer);
             this.newPlayer = {};
             document.body.querySelector(`.players .add input:first-child`).focus();
-
         }
     }
 });
